649d74c00f54d46ef52717b25cd4de75b1200e92
Ralph <bousamra.ralph@gmail.com>
2017-03-13 21:52:03 -0400

    even more broken build

--- app.js
+++ app.js
@@ -4,15 +4,45 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
+const pg = require('pg');
 
 var app = express();
 
-// view engine setup
+// connect to database
+var config = {
+  user: 'cs421g06', //env var: PGUSER 
+  database: 'cs421', //env var: PGDATABASE 
+  password: 'tisu612#', //env var: PGPASSWORD 
+  host: 'comp421.cs.mcgill.ca', // Server hosting the postgres database 
+  port: 5432, //env var: PGPORT 
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+};
+
+var pool = new pg.Pool(config);
+
+// pool.connect(function(err, client, done) {
+//   if(err) {
+//     return console.error('error fetching client from pool', err);
+//   }
+
+// client.query('SELECT * FROM car', function(err, result) {
+//     done(err);
+ 
+//     if(err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result.rows);
+//   });
+
+// });
+
+// view engine html setup
+var engine = require('consolidate');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +52,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
+
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+app.post('/', function(req, res){
+  var firstName = req.body.firstName;
+  pool.query('SELECT * FROM car', function(err, result) {
+    console.log("HELLOOOOO");
+    done(err);
+ 
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows);
+  });
+  res.render('index');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +89,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res){
+  var firstName = req.body.firstName;
+  console.log("I just got the ", firstName);
+});
+
 module.exports = router;
